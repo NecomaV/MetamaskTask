8b6a781cc90247eb8e4a03cc68b9f2dabf4cf5e6
NecomaV <73179504+NecomaV@users.noreply.github.com>
2023-04-06 18:27:23 +0600

    init: initial react files

---
+++ metamask-task/src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import './App.css';
+import { Person } from './components/Person';
+
+const App: FC = () => {
+ 
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ metamask-task/src/components/Person.tsx
@@ -0,0 +1,29 @@
+import { count } from 'console';
+import {ChangeEvent, FC,useState} from 'react';
+
+interface Props {
+    name: string;
+    age: number;
+    email: string;
+}
+
+export const Person: FC<Props> = ({name, age, email}) => {
+
+    const [country, setCountry] = useState<string>('') 
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCountry(event.target.value)
+    }
+  return (
+    <div>
+      <h1> {name} </h1>
+      <h1> {age} </h1>
+      <h1> {email} </h1>
+
+      <input placeholder='Input your country' onChange={handleChange} />
+
+      {country}
+
+    </div>
+  );
+}
